59d2d32418ae3c0d165c67b10d91d6fab5383cde
Malin Lüking <x_mallu@tetralith1.nsc.liu.se>
2022-07-04 18:42:19 +0200

    update scripts

--- plotting_gmx.py
+++ plotting_gmx.py
@@ -22,6 +22,45 @@ def plot_ana_rmsd(ana_f):
     
     rmsd = pd.concat([DBD, core, DNA])
     sns.lineplot ( data=rmsd, x = "Time (ps)", y = "RMSD (nm)", hue = "domain" )
+    return(rmsd)
+
+def plot_ana_rmsd_apo(ana_f):
+    """ Plot the root mean square deviation of atomic positions for core, DNA binding domains and DNA.
+
+    Parameters
+    ----------
+    ana_f: str
+	the folder with the .xvg files.
+    """
+
+    DBD = pd.read_csv( ana_f + "DBD_rmsd.xvg" , sep='\s+', skiprows=18, names=["Time (ps)", "RMSD (nm)"])
+    core = pd.read_csv( ana_f + "core_rmsd.xvg" , sep='\s+', skiprows=18, names=["Time (ps)", "RMSD (nm)"])
+    DBD['domain'] = ['DBD' for x in range ( len(DBD) ) ]
+    core['domain'] = ['core' for x in range ( len(core) ) ]
+    
+    rmsd = pd.concat([DBD, core])
+    sns.lineplot ( data=rmsd, x = "Time (ps)", y = "RMSD (nm)", hue = "domain" )
+    return(rmsd)
+
+def plot_ana_rmsd_2(ana_f):
+    """ Plot the root mean square deviation of atomic positions for core, DNA binding domains and DNA.
+
+    Parameters
+    ----------
+    ana_f: str
+	the folder with the .xvg files.
+    """
+
+    DNA = pd.read_csv( ana_f + "DNA_rmsd_2.xvg" , sep='\s+', skiprows=18, names=["Time (ps)", "RMSD (nm)"])
+    DBD = pd.read_csv( ana_f + "DBD_rmsd_2.xvg" , sep='\s+', skiprows=18, names=["Time (ps)", "RMSD (nm)"])
+    core = pd.read_csv( ana_f + "core_rmsd_2.xvg" , sep='\s+', skiprows=18, names=["Time (ps)", "RMSD (nm)"])
+    DNA['domain'] = ['DNA' for x in range ( len(DNA) )]
+    DBD['domain'] = ['DBD' for x in range ( len(DBD) ) ]
+    core['domain'] = ['core' for x in range ( len(core) ) ]
+    
+    rmsd = pd.concat([DBD, core, DNA])
+    sns.lineplot ( data=rmsd, x = "Time (ps)", y = "RMSD (nm)", hue = "domain" )
+    return(rmsd)
 
 def plot_ana_rmsf(ana_f):
     """ Plot the root mean square fluction of atomic positions for proteins and DNA.
@@ -47,6 +86,7 @@ def plot_ana_rmsf(ana_f):
     ax2.set_xlabel("Residue ID")
     
     fig.tight_layout()
+    return(protein,DNA)
 
 def plot_hb_num(ana_f):
     """ Plot the number of protein-protein and protein-DNA bonds along the trajectory.
@@ -71,4 +111,5 @@ def plot_hb_num(ana_f):
     ax2.set_ylabel("N (h-bonds)")
     ax2.set_xlabel("Time (ps)")
     
-    fig.tight_layout()	
+    fig.tight_layout()
+    return(protein,DNA)	
